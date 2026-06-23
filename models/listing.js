9ee40014f1cd605d0079554efa842b1e51c0bcc9
parthivsharma <parthivsharma020@gmail.com.exit.exit()git config --global  user.email parthivsharma020@gmail.com>
2026-06-23 21:22:43 +0530

    apply styling on page

--- models/listing.js
+++ models/listing.js
@@ -11,11 +11,11 @@ const listingSchema = new Schema({
     type: String,
     required: true,
   },
-  img: {
+  image: {
     type: String,
     default:"https://images.unsplash.com/photo-1777740718372-75b8d23d3cc0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
-      v === " "
+      v === " "== !v
         ? "https://images.unsplash.com/photo-1777740718372-75b8d23d3cc0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
